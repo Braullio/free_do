9210d066eed132220a5a00507587e26a56ac42a1
Braullio Vinnicius <braulliovg@gmail.com>
2019-07-18 18:27:33 -0300

    update(routes) add route lists (create/update/destroy)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'boards#index'
   resources :boards, :only => [:index, :create, :update, :destroy, :show]
+  resources :lists, :only => [:create, :update, :destroy]
 end
