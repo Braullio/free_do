913af9ffe6160ceb77216a6deda29e2dec32c41e
Braullio Vinnicius <braulliovg@gmail.com>
2019-07-19 16:48:39 -0300

    update(helper/board) aling

--- app/helpers/board_helper.rb
+++ app/helpers/board_helper.rb
@@ -1,6 +1,6 @@
 module BoardHelper
   def check_action_board
-    @board.present? ? ( @action = "update" ; @method = "patch" ; @title = @board.title ; @description = @board.description)
-                    : (@action = "create" ; @method = "post" ; @title = nil ; @description = nil)
+    @board.present? ? ( @action = "update" ; @method = "patch" ; @title = @board.title ; @description = @board.description )
+                    : ( @action = "create" ; @method = "post" ; @title = nil ; @description = nil )
   end
 end
