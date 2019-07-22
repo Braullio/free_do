086282486fcc6e6fe1314e697e5e4e64405875c1
Braullio Vinnicius <braulliovg@gmail.com>
2019-07-22 09:25:47 -0300

    update(routes) cards only create, update, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'boards#index'
   resources :boards, :only => [:index, :create, :update, :destroy, :show]
   resources :lists, :only => [:create, :update, :destroy]
+  resources :cards, :only => [:create, :update, :destroy]
 end
