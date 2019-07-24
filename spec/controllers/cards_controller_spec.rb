72817ab9818525c460af3cf75be56b8f1cdfad25
Braullio Vinnicius <braulliovg@gmail.com>
2019-07-24 15:42:50 -0300

    update(spec/controller/card) 1/2/3 is ok

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -10,25 +10,32 @@ RSpec.describe CardsController, type: :controller do
   }
 
   let(:valid_session) { {} }
+  let(:board) { Board.create( title: Faker::FunnyName.name , description: Faker::Lorem.sentence) }
+  let(:list)  { List.create( title: Faker::FunnyName.name , board_id: board.id) }
+  let(:card)  { Card.create( title: Faker::FunnyName.name , description: Faker::Lorem.sentence, list_id: list.id) }
 
   describe "POST #create" do
     context "with valid params" do
-      it "creates a new Card" do
+      it "creates a new List" do
         expect {
-          post :create, params: {card: valid_attributes}, session: valid_session
+          post :create, params: {title: Faker::FunnyName.name, list_id: list.id}, session: valid_session
         }.to change(Card, :count).by(1)
-      end
-
-      it "redirects to the created card" do
-        post :create, params: {card: valid_attributes}, session: valid_session
-        expect(Board.count).to redirect_to(Card.last)
+        expect(Board.count).to eq(1)
+        expect(List.count).to eq(1)
       end
     end
 
     context "with invalid params" do
-      it "returns a success response (i.e. to display the 'new' template)" do
-        post :create, params: {card: invalid_attributes}, session: valid_session
-        expect(response).to be_successful
+      it "not informad title" do
+        expect{
+          put :create, params: {list_id: list.id}, session: valid_session
+        }.to raise_error(ActiveRecord::StatementInvalid)
+      end
+
+      it "not informad list_id" do
+        expect{
+          put :create, params: {title: Faker::FunnyName.name}, session: valid_session
+        }.to change(List, :count).by(0)
       end
     end
   end
